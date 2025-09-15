4a842e8b86992ccfd65d476e78affd6c759603d8
Måns Asplund <56257484+nudplas75@users.noreply.github.com>
2025-09-15 13:18:38 +0200

    Text change

--- Exercise/Exercise_4a.py
+++ Exercise/Exercise_4a.py
@@ -9,8 +9,4 @@ accuracy = (TP + TN) / (TP + TN + FP + FN)
 
 print(f" {accuracy:.3f}")
 
-# This is a good model mathematically, but statistical answers
-# where the outcome is below 1% give the person interpreting the
-# answer very little information. I had chosen a graphical presentation
-# wich, despite the low precentage, would have given an answer that
-# is easier to interpret
+# Det beror på i vilken situation det gäller.
